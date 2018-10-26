b51280509d200e91b78c78cb4af7a6ec44d837b2
Nathan Sinclair <nathansinclair@Nathans-Mini.fritz.box>
2018-10-27 10:10:22 +1300

    ios comimt

---
+++ ios/equipapp/equipapp/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  equipapp
+//
+//  Created by Nathan Sinclair on 27/10/18.
+//  Copyright © 2018 Nathan Sinclair. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
